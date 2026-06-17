f633cbda8f91d1ebedbebd43515c076c4f117f2a
gabrielmanuel3129957-wq <gabrielmanuel3129957@gmail.com>
2026-06-17 06:14:30 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,8 @@
 
 # Substitui o número zero pelo teu ID de desenvolvedor real do 2Captcha
 # Podes encontrar este ID no teu painel de parceiro do 2Captcha
-DEVELOPER_ID = 0  
+DEVELOPER_ID = 5771
 
 # Esta variável vai guardar a chave do teu cliente (ele insere na interface)
 USER_CAPTCHA_KEY = ""
+
